3bb3e7f98a2008084a095399b6ab289384058526
lucazin <bergamo86@gmail.com>
2021-01-03 15:44:08 -0300

    apigateway zuul created

---
+++ service_api-gateway-zuul/src/main/java/com/devsuperior/service_apigatewayzuul/ServiceApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.service_apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApiGatewayZuulApplication.class, args);
+    }
+
+}
