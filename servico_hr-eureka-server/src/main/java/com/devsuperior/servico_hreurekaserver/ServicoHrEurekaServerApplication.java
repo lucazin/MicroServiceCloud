0cd7333c0fd86934fadc4ea6f5b564f805f3886a
lucazin <bergamo86@gmail.com>
2021-01-03 14:44:03 -0300

    eureka server

---
+++ servico_hr-eureka-server/src/main/java/com/devsuperior/servico_hreurekaserver/ServicoHrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.servico_hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoHrEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicoHrEurekaServerApplication.class, args);
+    }
+
+}
---
+++ servico_hr-eureka-server/src/test/java/com/devsuperior/servico_hreurekaserver/ServicoHrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.servico_hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoHrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
