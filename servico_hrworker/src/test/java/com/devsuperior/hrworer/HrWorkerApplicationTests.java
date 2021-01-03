cb26833f569164e07eacd6c94d0b8e5215e4784e
lucazin <bergamo86@gmail.com>
2021-01-03 10:22:47 -0300

    Project HRworker created

---
+++ servico_hrworker/src/test/java/com/devsuperior/hrworer/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrworer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
