62b44dafaa6ee423198687f8c9e1125c787ae2e5
lucazin <bergamo86@gmail.com>
2021-01-06 21:54:05 -0300

    User repo, user resource zull config

--- hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
@@ -13,7 +13,10 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
 
 
---
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -0,0 +1,31 @@
+package com.devsuperior.hruser.resources;
+
+import com.devsuperior.hruser.entities.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id)
+    {
+        User obj = userRepository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findById(@RequestParam String email)
+    {
+        User obj = userRepository.findByEmail(email);
+        return ResponseEntity.ok(obj);
+    }
+
+}
