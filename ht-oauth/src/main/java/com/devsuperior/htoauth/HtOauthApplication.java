b0928236c482aec2cd0a1da5d062daabacc627c7
lucazin <bergamo86@gmail.com>
2021-01-06 21:58:27 -0300

    project oauth created

---
+++ ht-oauth/src/main/java/com/devsuperior/htoauth/HtOauthApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.htoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtOauthApplication.class, args);
+    }
+
+}
---
+++ ht-oauth/src/test/java/com/devsuperior/htoauth/HtOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.htoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
